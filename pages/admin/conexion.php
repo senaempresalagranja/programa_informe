a7918ffeed3fb741ffd1316e6351f01ce5aa662d
Luis Alejandro Muñoz Garcia <sena.empresa2012@gmail.com>
2024-07-09 10:44:04 -0500

    estructura del aplicativo

---
+++ pages/admin/conexion.php
@@ -0,0 +1,9 @@
+<?php 
+
+$local="localhost";
+$usuario="bootcamp_informe_maestro";
+$contraseña="informe_maestro";
+$bd="bootcamp_informe_maestro";
+$conexion=mysqli_connect($local,$usuario,$contraseña,$bd);
+
+ ?>
